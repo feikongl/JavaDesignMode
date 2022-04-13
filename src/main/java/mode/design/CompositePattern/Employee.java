a3c0d00c305132b628d1c6f5db3a7cac8d8efedd
fiekong <feikong@shouxin168.com>
2022-04-13 22:25:09 +0800

    CompositePattern

---
+++ src/main/java/mode/design/CompositePattern/Employee.java
@@ -0,0 +1,41 @@
+package mode.design.CompositePattern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @time 2022/4/13 10:18 下午
+ * @Author feikong
+ */
+public class Employee {
+    private String name;
+    private String dept;
+    private int salary;
+    private List<Employee> subordinates;
+
+    //构造函数
+    public Employee(String name,String dept, int sal) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = sal;
+        subordinates = new ArrayList<Employee>();
+    }
+
+    public void add(Employee e) {
+        subordinates.add(e);
+    }
+
+    public void remove(Employee e) {
+        subordinates.remove(e);
+    }
+
+    public List<Employee> getSubordinates(){
+        return subordinates;
+    }
+
+    public String toString(){
+        return ("Employee :[ Name : "+ name
+                +", dept : "+ dept + ", salary :"
+                + salary+" ]");
+    }
+}
